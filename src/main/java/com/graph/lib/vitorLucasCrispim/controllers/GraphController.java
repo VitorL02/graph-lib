47549435c82ffb24562391513a514f86551a7c3a
Vitor Lucas <vitorcrispim@yahoo.com.br>
2024-04-03 16:26:48 -0300

    iniciando o projeto

---
+++ src/main/java/com/graph/lib/vitorLucasCrispim/controllers/GraphController.java
@@ -0,0 +1,25 @@
+package com.graph.lib.vitorLucasCrispim.controllers;
+
+import com.graph.lib.vitorLucasCrispim.services.GraphService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/graph")
+public class GraphController {
+
+    @Autowired
+    private GraphService graphService;
+
+    @PostMapping(path = "/upload",consumes = {"multipart/form-data"})
+    public ResponseEntity upload(@RequestParam(value = "file") MultipartFile file) throws Exception {
+
+        return ResponseEntity.status(HttpStatus.OK).body("Processamento iniciado. Aguarde!");
+    }
+}
---
+++ src/test/java/com/graph/lib/vitorLucasCrispim/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graph.lib.vitorLucasCrispim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
